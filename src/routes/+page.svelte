aa69a8b62dcf601271069bf812242619bc6fb491
Viktor Holta <viktor.holta@outlook.com>
2025-06-13 16:28:43 +0200

    Read file and use contents during deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import { PUBLIC_FAKE_CNAME } from '$env/static/public';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>Public static environement variables:</p>
+<ul>
+	<li>PUBLIC_FAKE_CNAME="{PUBLIC_FAKE_CNAME}"</li>
+</ul>
